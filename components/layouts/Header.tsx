75caca44301b8e71cf464a9ba652ba73799127fb
원동휘 <ehdehd1289@naver.com>
2025-07-13 20:03:05 +0900

    feat: 블로그 상세페이지 구현

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="bg-background sticky top-0 z-50 border-b">
-      <div className="container flex h-14 items-center px-4">
+      <div className="container flex h-[var(--height-header)] items-center px-4">
         <Link href="/" className="text-xl font-semibold">
           <span className="font-bold">원동휘 블로그</span>
         </Link>
