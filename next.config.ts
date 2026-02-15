b57ae9f2dc8196f394d27175e20a45db0306aa0d
wondonghwi <ehdehd1289@naver.com>
2026-02-16 08:45:18 +0900

    chore(config): allow notion image host

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,9 @@ const nextConfig: NextConfig = {
       {
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
       },
+      {
+        hostname: 'www.notion.so',
+      },
     ],
   },
 };
